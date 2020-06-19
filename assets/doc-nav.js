35594ab74a0eb5c53528fd9564f2b745e8f7d3c7
Nathan Bitcheno <Nathan@pdnz.co.nz>
2020-06-19 12:38:50 +1200

    responsive doc menu

--- assets/doc-nav.js
+++ assets/doc-nav.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function topNav() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+function myFunction() {
+    var x = document.getElementById("myDocnav");
+    if (x.className === "doc-nav") {
       x.className += " responsive";
     } else {
-      x.className = "topnav";
+      x.className = "doc-nav";
     }
   }
